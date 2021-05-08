fc87bc11152a033036a66bac0095dec25b22265e
Valery Koretsky <54971071+valkorlive@users.noreply.github.com>
2021-05-08 21:14:47 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,19 +2,19 @@
 
 $config = [
     'trello' => [
-        'key' => 'd1930619b5a0f20fbb6ed782a9cf83af',
-        'token' => '41daf1dd8d614e82bbab7262fcd4afa66173f7622fead769e8990875f32bb7fe',
+        'key' => '',
+        'token' => '',
     ],
 
     'todoist' => [
-        'token' => 'c6422fcfbc6d646227fafde790ae9386f4763c84',
+        'token' => '',
     ],
 
     'db' => [
         'host' => 'localhost',
-        'name' => 'trello_todoist_app',
-        'user' => 'root',
-        'password' => 'root',
+        'name' => '',
+        'user' => '',
+        'password' => '',
         'charset' => 'utf-8',
     ],
 ];
